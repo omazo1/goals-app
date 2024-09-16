94213918cc0d4359141a863c0faf880e53bcbcca
OmairKhan <techwithomair@gmail.com>
2024-09-15 22:27:07 -0400

    Working with omponents

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
+import GoalList from "./Components/GoalList/GoalList";
 
 const App = () => {
-  //JSX Code - Always use JSX code. Its easy to write the code and understand
+  
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul className="goal-list">
-        <li>Create React Application</li>
-        <li>Learn full stack tools and technologies</li>
-        <li>Complete assignments, quizzes and Tests</li>
-      </ul>
+      <GoalList />
+      
     </div>
   );
 };
---
+++ src/Components/GoalList/GoalList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./GoalList.css";
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Create React Application</li>
+      <li>Learn full stack tools and technologies</li>
+      <li>Complete assignments, quizzes and Tests</li>
+    </ul>
+  );
+  
+};
+
+export default GoalList;
