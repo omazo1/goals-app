3b6bf64441163d6044b195e504f5808e72fa3a27
OmairKhan <techwithomair@gmail.com>
2024-09-15 22:09:56 -0400

    Changed the code in App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  //JSX Code
-  return <h1 title='Welcome Message'> Hello friends, This is my first react application.</h1>
-
-  //convert JSX code to React code
-  return React.createElement('h1', {title:'Welcome Message'}, 'Hello Freinds, Welcome to COMP229')
+  //JSX Code - Always use JSX code. Its easy to write the code and understand
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <ul className="goal-list">
+        <li>Create React Application</li>
+        <li>Learn full stack tools and technologies</li>
+        <li>Complete assignments, quizzes and Tests</li>
+      </ul>
+    </div>
+  );
 };
-
-
 
 export default App;
